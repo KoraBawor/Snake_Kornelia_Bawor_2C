ac176df936e7d9ca21ecec7f0c68c14ea6f95033
Kejbix <117931442+kuryer@users.noreply.github.com>
2026-01-08 10:06:46 +0100

    snek update

---
+++ Assets/Menumanager.cs
@@ -0,0 +1,17 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menumanager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quitgame()
+    {
+        Application.Quit();
+    }
+}
+
--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -8,31 +8,31 @@ public class PauseMenu : MonoBehaviour
     public static bool GameIsPaused = false;
 
     public GameObject pauseMenuUI;
-   
+
 
     // Update is called once per frame
-    void Update()   {
+    void Update() {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (GameIsPaused)
-            
+
                 Resume();
             else
-            
-                
+
+
                 Pause();
-                
-            
+
+
         }
     }
 
-   public void Resume ()
+    public void Resume()
     {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
-
+    
     void Pause()
     {
         pauseMenuUI.SetActive(true);
